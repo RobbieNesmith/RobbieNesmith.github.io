85112ccd3d62b15c54ffcfea52899b39cda4a4dd
bobberto1995 <bobberto1995@gmail.com>
2017-05-31 13:00:32 -0500

    arrow keys control slideshow, added blender cheeto images

--- slideshow/Slideshow.js
+++ slideshow/Slideshow.js
@@ -1,1 +1,27 @@
-alert("Slideshow")
+var numImages = 2;
+var curImage = 0;
+
+function foobar(e)
+{
+  slide = document.getElementById("slide");
+  switch (e.keyCode)
+  {
+    case 37:
+      curImage--;
+      break;
+    case 39:
+      curImage++;
+      break;
+  }
+  if(curImage <= -1)
+  {
+    curImage += numImages;
+  }
+  if(curImage >= numImages)
+  {
+    curImage -= numImages;
+  }
+  slide.src = "img/" + curImage + ".jpg";
+}
+
+window.addEventListener("keydown", foobar, false);
