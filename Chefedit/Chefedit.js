f35a7204540b7b355409d010de39819b53ad7fcc
bobberto1995 <bobberto1995@gmail.com>
2017-11-12 10:04:28 -0800

    Add right-click blocks, middle click selection
    Right click is used for "secondary" block or eraser. Middle click is used
    to select a block for placement with left click.

--- Chefedit/Chefedit.js
+++ Chefedit/Chefedit.js
@@ -1,8 +1,7 @@
-var tileNum = 0;
-
-function draw() {
-  ellipse(50,50,80,80);
-}
+var tileNum = 1;
+var rcTile = 0;
+var isDraggingWs = false;
+var isDraggingRc = false;
 
 function setup() {
   tp = document.getElementById('tilepalette');
@@ -11,7 +10,8 @@ function setup() {
     e = document.createElement('div');
     im = document.createElement('div');
     e.className = "tileholder";
-    e.setAttribute('onclick', 'setSelected(this);');
+    e.setAttribute('onclick', 'setSelected(this, 0);');
+    e.setAttribute('oncontextmenu', 'setSelected(this, 1)');
     im.className = "tileimage";
     setBG(im,i);
     e.appendChild(im);
@@ -23,33 +23,112 @@ function setup() {
     }
     tp.appendChild(e);
   }
+  setSelected(tp.children[0], 1);
+  setSelected(tp.children[1], 0);
   
   for (i = 0; i < 20 * 15; i++) {
     tl = document.createElement('div');
     tl.className = "tileimage";
-    tl.setAttribute('onclick', 'setBG(this,tileNum);');
     ws.appendChild(tl);
   }
-  tp.children[0].className = "tileholder selected";
+  ws.setAttribute('onmousedown', 'wsMouseDown(this, event)');
+  ws.setAttribute('onmouseup', 'wsMouseUp()');
+  ws.setAttribute('onmouseleave', 'wsMouseUp()');
+  ws.setAttribute('onmousemove', 'wsMouseMove(this, event)');
 }
 
-function setSelected(el) {
+function wsMouseDown(el, ev) {
+  if (ev.button == 0) {
+    isDraggingWs = true;
+    placeAtCoords(el, x, y, tileNum);
+  } else if (ev.button == 1) {
+    getAtCoords(el, x, y);
+  } else if (ev.button == 2) {
+    isDraggingRc = true;
+    placeAtCoords(el, x, y, rcTile);
+  }
+}
+
+function wsMouseUp() {
+  isDraggingWs = false;
+  isDraggingRc = false;
+}
+
+function wsMouseMove(el, ev) {
+  x = ev.pageX - el.offsetLeft;
+  y = ev.pageY - el.offsetTop;
+  if (isDraggingWs) {
+    placeAtCoords(el, x, y, tileNum);
+  } else if (isDraggingRc) {
+    placeAtCoords(el, x, y, rcTile);
+  }
+}
+
+function placeAtCoords(el, x, y, tn) {
+  if (x >= 0 && x < 640 && y >= 0 && y < 480) {
+    idx = Math.floor(y / 32) * 20 + Math.floor(x / 32);
+    setBG(el.children[idx],tn);
+  }
+}
+
+function getAtCoords(el, x, y) {
+  if (x >= 0 && x < 640 && y >= 0 && y < 480) {
+    idx = Math.floor(y / 32) * 20 + Math.floor(x / 32);
+    px = el.children[idx].style.backgroundPositionX;
+    py = el.children[idx].style.backgroundPositionY;
+    if (px == "") {
+      px = 0;
+    } else {
+      px = -parseInt(px.substring(0, px.length - 2)) / 32;
+    }
+    if (py == "") {
+      py = 0;
+    } else {
+      py = -parseInt(py.substring(0, py.length - 2)) / 32;
+    }
+    setSelectedByIndex(px + 16 * py, 0);
+  }
+}
+
+function setSelected(el, mb) {
   tp = document.getElementById('tilepalette');
   ch = tp.children;
   for (i = 0; i < ch.length; i++) {
-    ch[i].className = "tileholder";
+    if (mb == 0) {
+      ch[i].classList.remove("selected");
+    } else if (mb == 1) {
+      ch[i].classList.remove("rightsel");
+    }
   }
-  el.className = "tileholder selected";
-  tileNum = 0;
+  if (mb == 0) {
+    el.className += " selected";
+    tileNum = 0;
+  } else if (mb == 1) {
+    el.className += " rightsel";
+    rcTile = 0;
+  }
   while (null != el.previousElementSibling) {
-    tileNum++;
+    if (mb == 0) {
+      tileNum++;
+    } else {
+      rcTile++;
+    }
     el = el.previousElementSibling;
   }
+}
+
+function setSelectedByIndex(index, mb) {
+  tp = document.getElementById('tilepalette');
+  setSelected(tp.children[index], mb)
 }
 
 function setBG(el,idx) {
   x = idx % 16;
   y = Math.floor(idx / 16);
   el.style.backgroundPosition = x * -32 + 'px ' + y * -32 + 'px';
-  console.log(idx);
+}
+
+function getChar (event){
+  var keyCode = ('which' in event) ? event.which : event.keyCode;
+  alert ("The Unicode key code is: " + keyCode);
 }
