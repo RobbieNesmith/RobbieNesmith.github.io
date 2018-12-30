ffc78d941955294a0b585a2719e8e620cf40d55c
Robbie Nesmith <bobberto1995@gmail.com>
2018-12-30 15:12:45 -0800

    Add lights out game
    Built from react. The index.html has been modified to relatively point
    to the required files. I will upload the source in a different repo.

---
+++ lightsout/precache-manifest.6a7d853b47513b29cf5fcfd0d93a2c8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b47f3c15a85fb48de1fd",
+    "url": "/static/js/main.b47f3c15.chunk.js"
+  },
+  {
+    "revision": "9d65a0d5e5a889113337",
+    "url": "/static/js/1.9d65a0d5.chunk.js"
+  },
+  {
+    "revision": "b47f3c15a85fb48de1fd",
+    "url": "/static/css/main.b2697efd.chunk.css"
+  },
+  {
+    "revision": "1f781f80e83780fee83c36544e15625b",
+    "url": "/index.html"
+  }
+];
