c6165813c43ac9d3e63799fa08030e3d52d61e6e
Robbie Nesmith <bobberto1995@gmail.com>
2019-01-28 08:53:11 -0800

    Add DMC thread GIMP palette generator

---
+++ dmc-palette/precache-manifest.b670664b0dacae6569f4d2e89def848c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3f60d64e83f226256945",
+    "url": "/static/js/main.3f60d64e.chunk.js"
+  },
+  {
+    "revision": "9ef807963481eb41277b",
+    "url": "/static/js/1.9ef80796.chunk.js"
+  },
+  {
+    "revision": "3f60d64e83f226256945",
+    "url": "/static/css/main.b992fb16.chunk.css"
+  },
+  {
+    "revision": "d94156381a8c01e97bb2e64d9964bf37",
+    "url": "/index.html"
+  }
+];
