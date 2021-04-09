29fd5a18c029f5a50c6bb680bc65d78d66ae9102
Robbie Nesmith <robbie@newbo.co>
2021-04-09 09:03:35 -0500

    Add export function to web audio processing demo

--- demos/WebAudioProcessing/script.js
+++ demos/WebAudioProcessing/script.js
@@ -8,13 +8,17 @@ let diffElement;
 
 let diffOverTime = 0;
 let diffChart = [];
+let diffLog = [];
+let diffOverTimeLog = [];
 
 const MAX_DOT = 2000;
+
+let running = false;
 
 async function setupContext() {
   const mic = await getInput();
   const source = audioCtx.createMediaStreamSource(mic);
-  
+
   source.connect(splitter)
   splitter.connect(analyserL, 0);
   splitter.connect(analyserR, 1);
@@ -49,55 +53,86 @@ function setup() {
 	dbElementL = document.getElementById("decibelsL");
 	dbElementR = document.getElementById("decibelsR");
   diffElement = document.getElementById("diff");
-  
+
   setupContext();
-  
-  createCanvas(640, 380);
+
+  let canvas = createCanvas(640, 380);
+  canvas.parent("canvasHolder");
+  frameRate(5);
 }
 
 function draw() {
-  analyserL.getFloatFrequencyData(loudnessL);
-  analyserR.getFloatFrequencyData(loudnessR);
-  
-  const leftVolume = Math.max(...loudnessL);
-  const rightVolume = Math.max(...loudnessR);
-  const diffVolume = rightVolume - leftVolume;
-  if (diffVolume) {
-    diffOverTime += diffVolume;
+  if (running) {
+    analyserL.getFloatFrequencyData(loudnessL);
+    analyserR.getFloatFrequencyData(loudnessR);
+
+    const leftVolume = Math.max(...loudnessL);
+    const rightVolume = Math.max(...loudnessR);
+    const diffVolume = rightVolume - leftVolume;
+    if (diffVolume) {
+      diffOverTime += diffVolume;
+    }
+
+    if (diffOverTime > MAX_DOT) {
+      diffOverTime = MAX_DOT;
+    }
+
+    if (diffOverTime < -MAX_DOT) {
+      diffOverTime = -MAX_DOT;
+    }
+
+    if (diffChart.length < 600) {
+      diffChart.push(diffVolume);
+    } else {
+      diffChart = [];
+    }
+
+    diffLog.push(diffVolume);
+    diffOverTimeLog.push(diffOverTime);
+
+    dbElementL.innerText = leftVolume;
+    dbElementR.innerText = rightVolume;
+    diffElement.innerText = diffVolume;
+    background(200,200,255);
+    rect(20, 20, 600, 100);
+    rect(20, 140, 600, 100);
+    rect(20, 260, 600, 100);
+
+    const diffX = map(diffVolume, -30, 30, 20, 620);
+    line(diffX, 20, diffX, 120);
+    const dotX = map(diffOverTime, -MAX_DOT, MAX_DOT, 20, 620);
+    line(dotX, 140, dotX, 240);
+
+    for (let i = 0; i < diffChart.length; i++) {
+      const c = lerpColor(color(255, 0, 0), color(0, 0, 255), map(diffChart[i], -30, 30, 0, 1));
+      push();
+      stroke(c);
+      line(20 + i, 260, 20 + i, 360);
+      pop();
+    }
   }
-  
-  if (diffOverTime > MAX_DOT) {
-    diffOverTime = MAX_DOT;
+}
+
+function toggleRunning() {
+  running = !running;
+  let buttonText = running ? "Stop" : "Start";
+  let buttonClass = running ? "btn btn-danger" : "btn btn-success";
+  let buttonElement = document.getElementById("runToggle");
+  buttonElement.innerText = buttonText;
+  buttonElement.className = buttonClass;
+}
+
+function exportData() {
+  let output = "Volume difference,Volume difference smoothed\r\n";
+  for (let i = 0; i < diffLog.length; i++) {
+    output += `${diffLog[i]},${diffOverTimeLog[i]}\r\n`;
   }
-  
-  if (diffOverTime < -MAX_DOT) {
-    diffOverTime = -MAX_DOT;
-  }
-  
-  if (diffChart.length < 600) {
-    diffChart.push(diffVolume);
-  } else {
-    diffChart = [];
-  }
-  
-  dbElementL.innerText = leftVolume;
-  dbElementR.innerText = rightVolume;
-  diffElement.innerText = diffVolume;
-  background(200,200,255);
-  rect(20, 20, 600, 100);
-  rect(20, 140, 600, 100);
-  rect(20, 260, 600, 100);
-  
-  const diffX = map(diffVolume, -30, 30, 20, 620);
-  line(diffX, 20, diffX, 120);
-  const dotX = map(diffOverTime, -MAX_DOT, MAX_DOT, 20, 620);
-  line(dotX, 140, dotX, 240);
-  
-  for (let i = 0; i < diffChart.length; i++) {
-    const c = lerpColor(color(255, 0, 0), color(0, 0, 255), map(diffChart[i], -30, 30, 0, 1));
-    push();
-    stroke(c);
-    line(20 + i, 260, 20 + i, 360);
-    pop();
-  }
+
+  let dlLink = document.createElement('a');
+  dlLink.href = "data:text/csv;charset=utf-8," + encodeURI(output);
+  dlLink.style.display = "none";
+  dlLink.download = "meeting.csv";
+  document.body.appendChild(dlLink);
+  dlLink.click();
+  document.body.removeChild(dlLink);
 }
