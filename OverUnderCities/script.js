002780edef8b4c23e03b4163e3ec7cd83ce4d353
Robbie Nesmith <bobberto1995@gmail.com>
2025-04-05 22:10:20 -0500

    Add start for OverUnderCities

---
+++ OverUnderCities/script.js
@@ -0,0 +1,60 @@
+const CLOSE_THRESHOLD = 5;
+
+let city1Map = null;
+let city2Map = null;
+
+let worldCities = null;
+
+let direction = null;
+let directions = ["North", "South", "East", "West"];
+
+function closeLat(cityA, cityB) {
+  return Math.abs(cityA.geometry.coordinates[1] - cityB.geometry.coordinates[1]) < CLOSE_THRESHOLD;
+}
+
+function closeLon(cityA, cityB) {
+  return Math.abs(cityA.geometry.coordinates[0] - cityB.geometry.coordinates[0]) < CLOSE_THRESHOLD;
+}
+
+async function setup() {
+  const direction = directions[Math.floor(Math.random() * directions.length)];
+  const directionText = document.getElementById("direction");
+  directionText.innerText = direction;
+  const worldCitiesResp = await fetch("World_Cities.geojson");
+  worldCities = await worldCitiesResp.json();
+
+  const featuresList = worldCities.features;
+
+  const city1 = featuresList[Math.floor(Math.random() * featuresList.length)];
+
+  const filteredCities = featuresList.filter(city => {
+    if (direction === "North" || direction === "South") {
+      return closeLat(city, city1);
+    } else {
+      return closeLon(city, city1);
+    }
+  });
+
+  const city2 = filteredCities[Math.floor(Math.random() * filteredCities.length)];
+
+  const city1Coords = [city1.geometry.coordinates[1], city1.geometry.coordinates[0]];
+  const city2Coords = [city2.geometry.coordinates[1], city2.geometry.coordinates[0]];
+
+  const city1NameText = document.getElementById("city1-name");
+  const city2NameText = document.getElementById("city2-name");
+
+  city1NameText.innerText = `${city1.properties.CITY_NAME}, ${city1.properties.ADMIN_NAME}, ${city1.properties.CNTRY_NAME}`;
+  city2NameText.innerText = `${city2.properties.CITY_NAME}, ${city2.properties.ADMIN_NAME}, ${city2.properties.CNTRY_NAME}`;
+
+  city1Map = L.map("city1").setView(city1Coords, 10);
+  city2Map = L.map("city2").setView(city2Coords, 10);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(city1Map);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(city2Map);
+}
+
