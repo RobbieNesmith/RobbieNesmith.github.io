5c88fc5001d95a3be55775db896063c74b9e8103
Robbie Nesmith <bobberto1995@gmail.com>
2025-07-10 20:47:54 -0400

    Add HPGL output

--- p5/PlaneFun/sketch.js
+++ p5/PlaneFun/sketch.js
@@ -4,10 +4,14 @@ let windowMin;
 let itersSlider;
 let ratioSlider;
 let countSlider;
+let outputDiv;
+let hpglButton;
 let center;
 let lastIters = 0;
 let lastRatio = 0;
 let lastCount = 0;
+let showHpgl = "DRAW";
+let lastShowHpgl = "DRAW";
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
@@ -23,7 +27,7 @@ function circlePart(location, vector, scale, iter) {
   const newLocation = location.copy();
   newLocation.add(scaledVector);
   const newScale = scale * ratioSlider.value();
-  circle(newLocation.x, newLocation.y, newScale);
+  renderCircle(newLocation.x, newLocation.y, newScale, showHpgl);
   if (iter < itersSlider.value()) {
       for (let i = 1; i < countSlider.value(); i++) {
           const newVector = vector.copy();
@@ -38,22 +42,24 @@ function draw() {
   const startingSize = windowMin / 4;
   if(lastRatio === ratioSlider.value() &&
   lastIters === itersSlider.value() &&
-  lastCount === countSlider.value()) {
+  lastCount === countSlider.value() &&
+  lastShowHpgl === showHpgl) {
     return;
   }
+  outputDiv.elt.innerText = "";
   background(...BACKGROUND_COLOR);
-  circle(center.x, center.y, startingSize);
+  renderCircle(center.x, center.y, startingSize, showHpgl);
   
   for (let i = 0; i < countSlider.value(); i++) {
     const newVector = createVector(0, (1 + ratioSlider.value()) / 2);
     newVector.rotate(i * 2 * PI / countSlider.value());
     circlePart(center, newVector, startingSize, 1);
   }
-  
-  //circlePart(center, createVector(0,(1 + ratioSlider.value()) / 2), startingSize, 1);
+
   lastRatio = ratioSlider.value();
   lastIters = itersSlider.value();
   lastCount = countSlider.value();
+  lastShowHpgl = showHpgl;
 }
 
 function windowResized() {
@@ -71,7 +77,18 @@ function setUpGui() {
   ratioSlider = createSlider(0.1, 0.9, 0.5, 0.01);
   itersSlider = createSlider(1, 10, 5, 1);
   countSlider = createSlider(2, 10, 4, 1);
+  outputDiv = createDiv();
+  hpglButton = createButton("Toggle HPGL Output");
+  hpglButton.mousePressed(toggleHpgl);
   updateGui();
+}
+
+function toggleHpgl() {
+  if (showHpgl === "DRAW") {
+    showHpgl = "BOTH";
+  } else {
+      showHpgl = "DRAW";
+  }
 }
 
 function updateGui() {
@@ -81,4 +98,20 @@ function updateGui() {
   itersSlider.size(width - 32);
   countSlider.position(16, height - 96);
   countSlider.size(width - 32);
+  outputDiv.position(0, 0);
+  outputDiv.size(width, height - 128);
+  outputDiv.elt.className = "output";
+  hpglButton.position(16, height - 128);
+  hpglButton.size(width - 32);
+}
+
+function renderCircle(x, y, diameter, mode="DRAW") {
+  if (mode === "DRAW") {
+    circle(x, y, diameter);
+  } else if (mode === "HPGL") {
+    outputDiv.elt.innerText += `CI ${x},${y},${diameter/2};\n`;
+  } else if (mode === "BOTH") {
+    circle(x, y, diameter);
+    outputDiv.elt.innerText += `CI ${x},${y},${diameter/2};\n`;
+  }
 }
