140413d1234b9bd4fa89f2937ba35ea4d79de3ca
Robbie Nesmith <bobberto1995@gmail.com>
2025-07-15 10:35:26 -0400

    Create, Edit, Delete sproings

--- p5/Sproings/sketch.js
+++ p5/Sproings/sketch.js
@@ -1,4 +1,7 @@
 const BACKGROUND_COLOR = [200, 200, 200];
+
+const TOUCH_SLOP = 10;
+
 const STATE_IDLE = "STATE_IDLE";
 const STATE_CREATE = "STATE_CREATE";
 const STATE_CONNECT = "STATE_CONNECT";
@@ -11,13 +14,18 @@ let nodes = [];
 let springs = [];
 
 let state = STATE_IDLE;
-let connectingNode = null;
+let activeNode = null;
 let clickStart = null;
+
+let grabButton;
+let editButton;
+let deleteButton;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	scaleContent();
   render();
+  setupGui();
 }
 
 function draw() {
@@ -42,9 +50,16 @@ function touchStarted() {
 }
 
 function mouseDragged() {
-    if (connectingNode) {
-        render();
-        line(connectingNode.position.x, connectingNode.position.y, mouseX, mouseY);
+    if (activeNode) {
+        if (state === STATE_CONNECT) {
+          render();
+          line(activeNode.position.x, activeNode.position.y, mouseX, mouseY);
+        }
+        if (state === STATE_MOVE) {
+            activeNode.position.x = mouseX;
+            activeNode.position.y = mouseY;
+            render();
+        }
     }
 }
 
@@ -57,40 +72,50 @@ function touchEnded() {
 }
 
 function handleClick() {
-    if (state === STATE_IDLE) {
-        clickStart = createVector(mouseX, mouseY);
-        state = STATE_CREATE;
-        for (let node of nodes) {
-            if (clickStart.dist(node.position) < node.weight) {
-                state = STATE_CONNECT;
-                connectingNode = node;
-            }
+    clickStart = createVector(mouseX, mouseY);
+    for (let node of nodes) {
+      if (clickStart.dist(node.position) < node.weight) {
+        if (state == STATE_IDLE) {
+          state = STATE_CONNECT;
         }
+        activeNode = node;
+      }
+    }
+    if (state === STATE_IDLE) {
+        state = STATE_CREATE;
     }
 }
 
 function handleRelease() {
+    const clickEnd = createVector(mouseX, mouseY);
     if (state === STATE_CREATE) {
         nodes.push({
           position: createVector(mouseX, mouseY),
           weight: 20
         });
     } else if (state === STATE_CONNECT) {
-        const clickEnd = createVector(mouseX, mouseY);
         for (let node of nodes) {
             if (clickEnd.dist(node.position) < node.weight) {
                 springs.push({
-                    a: connectingNode,
+                    a: activeNode,
                     b: node,
-                    springLength: connectingNode.position.dist(node.position),
+                    springLength: activeNode.position.dist(node.position),
                     springStiff: 0.5
                 });
             }
         }
+    } else if (state === STATE_DELETE) {
+       if (activeNode && clickEnd.dist(clickStart) < TOUCH_SLOP &&
+         clickEnd.dist(activeNode.position) < activeNode.weight) {
+           springs = springs.filter(sp => sp.a != activeNode && sp.b != activeNode);
+           nodes = nodes.filter(n => n != activeNode);
+       }
     }
     render();
-    connectingNode = null;
-    state = STATE_IDLE;
+    activeNode = null;
+    if (state === STATE_CREATE || state === STATE_CONNECT) {
+      state = STATE_IDLE;
+    }
 }
 
 function render() {
@@ -103,4 +128,36 @@ function render() {
     for (let node of nodes) {
         circle(node.position.x, node.position.y, node.weight);
     }
+}
+
+function setGrab() {
+    state = STATE_MOVE;
+}
+
+function setEdit() {
+    state = STATE_IDLE;
+}
+
+function setDelete() {
+    state = STATE_DELETE;
+}
+
+function setupGui () {
+  grabButton = createButton("Grab");
+  grabButton.mousePressed(setGrab);
+  editButton = createButton("Edit");
+  editButton.mousePressed(setEdit);
+  deleteButton = createButton("Delete");
+  deleteButton.mousePressed(setDelete);
+  updateGui();
+}
+
+function updateGui() {
+  const paddedSpace = width - 32;
+  grabButton.position(16, height - 32);
+  grabButton.size(paddedSpace / 3);
+  editButton.position(16 + paddedSpace / 3, height - 32);
+  editButton.size(paddedSpace / 3);
+  deleteButton.position(width - 16 - paddedSpace / 3, height - 32);
+  deleteButton.size(paddedSpace / 3);
 }
